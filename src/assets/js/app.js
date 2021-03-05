fa55ba00c571ad16f232ed3b561b22336585f6c0
Marat Minchik <devminrat@gmail.com>
2021-03-05 19:12:37 +0300

    added intro and video sections

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,9 @@
 //= ../../../node_modules/swiper/swiper-bundle.js
+//= ../../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js
 
 $(function () {
   //= components/swiper.js
+  //= components/fancybox.js
 
   console.log("file 3");
 });
